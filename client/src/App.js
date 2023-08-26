ee7027c2d1e14f0ce73d8e97a30c6ca41f090964
Eyüp Can Esen <eyupcanee@gmail.com>
2023-08-26 13:13:16 +0300

    Client Ve Server Tarafında Gerekli Dosyalar Oluşturuldu. Server Tarafında İhitiyaç Olabilecek npm packageler kuruldu.

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello From React!!!</h1>
+      <h1>I haven't been coding react and nodejs for a long time</h1>
+    </div>
+  );
+}
+
+export default App;
